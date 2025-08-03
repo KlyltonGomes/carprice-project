d593e55381ffdc48025dd1551e3190e4760774cf
Klylton Gomes <klyltongomes@gmail.com>
2025-08-03 16:09:10 -0300

    Correção da estrutura de codigo no main.py

--- carprice-scraper/main.py
+++ carprice-scraper/main.py
@@ -1,40 +1,34 @@
+import asyncio
 import yaml
-from fastapi import FastAPI, HTTPException
-from pathlib import Path
-
-from models import CarPrice
-from swagger_config import swagger_config
-from playwright_handler import capturar_payload
 from parser import parse_batch
+from pathlib import Path
+from playwright_handler import capturar_payload
 
-# Configurações
-CONFIG_PATH = Path(__file__).parent.parent / "carprice-scraper.yaml"
+# Caminho do arquivo YAML
+CONFIG_PATH = Path(__file__).parent / "carprice-scraper.yaml"
 config = yaml.safe_load(CONFIG_PATH.read_text())
 
 rentcars_url = config["rentcars"]["url"]
 browser_type = config["rentcars"]["browser"]
+
 print(f"Scraping URL: {rentcars_url} usando {browser_type}")
 
-# FastAPI app
-app = FastAPI(
-    title=swagger_config["title"],
-    description=swagger_config["description"],
-    version=swagger_config["version"]
-)
-
-@app.get(
-    "/scraper",
-    summary="Dispara o scraper e retorna os dados",
-    response_model=list[CarPrice]
-)
 async def scraper():
+    # Chama a função que usa Playwright para capturar os dados
+    print("Scraper ativado! Loading...")
     payloads = await capturar_payload(rentcars_url)
 
-    if not payloads:
-        raise HTTPException(status_code=404, detail="Nenhum dado encontrado")
-
+    # Aqui você pode enviar para o RabbitMQ ou salvar em arquivo
+    print(f"Total payloads capturados: {len(payloads)}")
     carros = []
     for batch in payloads:
         carros.extend(parse_batch(batch))
 
-    return carros
+    print(f"Carros processados: {len(carros)}")
+    for carro in carros:
+        print(carro)
+
+    #return payloads
+
+if __name__ == "__main__":
+    asyncio.run(scraper())
